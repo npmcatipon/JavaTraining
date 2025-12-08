6b20ac92896e38e5a2c7d79ae14a7a91770f4cb0
npmcatipon <npmcatipon@bpi.com.ph>
2025-12-08 17:27:06 +0800

    Revert "updating files for module 2 activity 2"
    This reverts commit 3ea185f11107946ce2cb7395018b904bd29033b5.

---
+++ SeatWork2.java
@@ -0,0 +1,11 @@
+// M2 Activity2
+public class SeatWork2 {
+
+	public static void main(String[] args) {
+		WhiteBoard wb = new WhiteBoard();
+		int result = wb.addNumbers(5, 3);
+		System.out.println("The total is: " + String.valueOf(result));
+
+	}
+
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,9 @@
+// M2 Activity2
+public class WhiteBoard {
+	int addNumbers(int addend1, int addend2) {
+		int total;
+		total = addend1 + addend2;
+		
+		return total;
+	}
+}
