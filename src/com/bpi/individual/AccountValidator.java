38bcc1e64a341637a3b565bd0bd5799b5d4408ae
npmcatipon <npmcatipon@IBMQ.headoffice.corp.bpi.com.ph>
2026-01-12 11:37:41 +0800

    changed path to a java package

---
+++ src/com/bpi/individual/AccountValidator.java
@@ -0,0 +1,41 @@
+package com.bpi.individual;
+public class AccountValidator {
+	static void validateAccountNumber(String accountNumber) throws Exception{
+		if (accountNumber == null) {
+			throw new NullPointerException("Cannot be null");
+		} else if (accountNumber.length() != 10) {
+			throw new Exception("Must be 10 digits");
+		} else {
+			System.out.println("Valid Account: " + accountNumber);
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			validateAccountNumber("1234567890");
+		} catch (NullPointerException e) {
+			System.out.println("Error: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		try {
+			validateAccountNumber("123");
+		} catch (NullPointerException e) {
+			System.out.println("Error: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		try {
+			validateAccountNumber(null);
+		} catch (NullPointerException e) {
+			System.out.println("Error: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		
+	}
+
+}
