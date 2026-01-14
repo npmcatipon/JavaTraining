d0e81e142063799114f82362254a0c2023ddc6e1
'npmcatipon' <'npmcatipon@bpi.com.ph'>
2026-01-14 20:13:06 +0800

    updated the logback config; keep 30 days of log; total size 1GB; max file size 100MB

--- src/com/bpi/m4/activity8/M4Activity8.java
+++ src/com/bpi/m4/activity8/M4Activity8.java
@@ -5,10 +5,11 @@ public class M4Activity8 {
 	public static void main(String[] args) {
 		BankAccount bac = new BankAccount();
 
-		BankAccount.runTest(()-> bac.deposit(5000),"Deposit");
+		BankAccount.runTest(() -> bac.deposit(5000),"Deposit");
 		BankAccount.runTest(() -> bac.withdraw(3000), "Withdrawal");
 		BankAccount.runTest(() -> bac.deposit(-500), "Deposit");
 		BankAccount.runTest(() -> bac.withdraw(20000), "Withdrawal");
 		BankAccount.runTest(() -> bac.deposit(60000), "Deposit");
+		
 	}
 }
