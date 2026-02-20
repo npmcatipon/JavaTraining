c9422c2c854848dc8acf88f2027af587a7b92a0e
npmcatipon <npmcatipon@bpi.com.ph>
2026-02-20 08:41:06 +0800

    Initial commit for Module 8 Activity 8.4
    Implementing @RequestParam

--- src/main/java/com/training/module8/controller/BookController.java
+++ src/main/java/com/training/module8/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,6 +49,13 @@ public class BookController {
 				.filter(b -> b.getId().equals(id))
 				.findFirst()
 				.orElse(null);
+	}
+	
+	@GetMapping("/search")
+	public List<Book> search(@RequestParam String title) {
+		return books.stream()
+				.filter(b -> b.getTitle().equals(title))
+				.toList();
 	}
 	
 }
