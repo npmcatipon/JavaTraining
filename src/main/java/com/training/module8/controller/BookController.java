9bb634ad7543fb534f782538bfbec56f06388c12
npmcatipon <npmcatipon@bpi.com.ph>
2026-02-20 08:14:27 +0800

    Updated Module 8 Activity 8.1
    Implementing GET

--- src/main/java/com/training/module8/controller/BookController.java
+++ src/main/java/com/training/module8/controller/BookController.java
@@ -3,7 +3,6 @@ package com.training.module8.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,22 +14,14 @@ import com.training.module8.model.Book;
 public class BookController {
 	
 	private static final List<Book> books = List.of(
-			new Book(1L,"Java 101"),
-			new Book(2L,"Java 102"),
-			new Book(3L,"Java 103")
+			new Book(1L,"Java 101","Author of Java 101"),
+			new Book(2L,"Java 102","Author of Java 102"),
+			new Book(3L,"Java 103","Author of Java 103")
 			);
 
 	@GetMapping
 	public List<Book> getAll() {
 		return books;
-	}
-	
-	@GetMapping("/{id}")
-	public Book getOne(@PathVariable Long id) {
-		return books.stream()
-				.filter(b -> b.getId().equals(id))
-				.findFirst()
-				.orElse(null);
 	}
 
 	
--- src/main/java/com/training/module8/model/Book.java
+++ src/main/java/com/training/module8/model/Book.java
@@ -2,28 +2,35 @@ package com.training.module8.model;
 
 public class Book {
 	
-	private String title;
 	private Long id;
-
+	private String title;
+	private String author;
+	
+	public Book(Long id, String title, String author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public Book(Long id,String string) {
-		this.id = id;
-		this.title = string;
-	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
 
 }
