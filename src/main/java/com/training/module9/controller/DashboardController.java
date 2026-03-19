50bc25aad5bab4b4a0323efba103ed1c4bfbfe8a
npmcatipon <npmcatipon@bpi.com.ph>
2026-03-19 23:07:56 +0800

    accessing current user. option 1: securitcontextholder

--- src/main/java/com/training/module9/controller/DashboardController.java
+++ src/main/java/com/training/module9/controller/DashboardController.java
@@ -1,6 +1,12 @@
 package com.training.module9.controller;
 
+import java.security.Principal;
+import java.util.Collection;
+
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +34,19 @@ public class DashboardController {
 	@GetMapping("/profile/username/{username}")
 	@PreAuthorize("#username == authentication.name")
 	public String getProfileByUserName(@PathVariable String username) {
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		if (auth != null && auth.isAuthenticated()) {
+			String uname = auth.getName();
+			Object principal = auth.getPrincipal();
+			Collection<? extends GrantedAuthority> roles = auth.getAuthorities();
+
+			System.out.println("username: " + uname);
+			System.out.println("principal: " + principal);
+			System.out.println("roles: " + roles);
+		}
+
 		return "Profile of " + username;
 	}
 }
