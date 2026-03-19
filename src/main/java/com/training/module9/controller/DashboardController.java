a8db3bfb6cead297ce38056b93d0727c5fb643cd
npmcatipon <npmcatipon@bpi.com.ph>
2026-03-19 23:12:55 +0800

    accessing current user. option 2: using @AuthenticationPrincipal

--- src/main/java/com/training/module9/controller/DashboardController.java
+++ src/main/java/com/training/module9/controller/DashboardController.java
@@ -6,7 +6,9 @@ import java.util.Collection;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,19 +36,11 @@ public class DashboardController {
 	@GetMapping("/profile/username/{username}")
 	@PreAuthorize("#username == authentication.name")
 	public String getProfileByUserName(@PathVariable String username) {
-
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-		if (auth != null && auth.isAuthenticated()) {
-			String uname = auth.getName();
-			Object principal = auth.getPrincipal();
-			Collection<? extends GrantedAuthority> roles = auth.getAuthorities();
-
-			System.out.println("username: " + uname);
-			System.out.println("principal: " + principal);
-			System.out.println("roles: " + roles);
-		}
-
 		return "Profile of " + username;
+	}
+
+	@GetMapping("/profile2")
+	public String getProfile2(@AuthenticationPrincipal UserDetails user) {
+		return "Hello, " + user.getUsername();
 	}
 }
