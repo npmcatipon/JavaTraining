71e775bb93ed536b355f34cdcb3823f769aae6dc
npmcatipon <npmcatipon@bpi.com.ph>
2026-04-07 12:03:29 +0800

    revise: UserMapper added ignore on roles; retain UsernameAlreadyExistException

--- src/main/java/com/bpi/training/module10/exception/GlobalHandlerException.java
+++ src/main/java/com/bpi/training/module10/exception/GlobalHandlerException.java
@@ -1,63 +1,14 @@
 package com.bpi.training.module10.exception;
 
-import com.bpi.training.module10.Module10Application;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalHandlerException {
-private final Module10Application module10Application;
-
-    GlobalHandlerException(Module10Application module10Application) {
-        this.module10Application = module10Application;
-    }
-
-@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ApiResponse> handleValidation(
-			MethodArgumentNotValidException ex,
-			HttpServletRequest req) {
-		String message = ex.getBindingResult()
-				.getFieldErrors()
-				.stream()
-				.findFirst()
-				.map(err -> err.getField() + ": " + err.getDefaultMessage())
-				.orElse("Invalid Request");
-
-		return ResponseEntity.badRequest().body(
-				ApiResponse.of(400,
-						"Bad Request",
-						message,
-						req.getRequestURI()));
-	}
-
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<ApiResponse> handleConstraintValidation(
-			ConstraintViolationException ex,
-			HttpServletRequest req) {
-		return ResponseEntity.badRequest().body(
-				ApiResponse.of(400,
-						"Bad Request",
-						ex.getMessage(),
-						req.getRequestURI()));
-	}
-
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ApiResponse> handleException(
-			Exception ex,
-			HttpServletRequest req) {
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-				ApiResponse.of(500,
-						"Internal Server Error",
-						"An unexpected error occur.",
-						req.getRequestURI()));
-	}
 
 	@ExceptionHandler(UsernameAlreadyExistException.class)
 	public ResponseEntity<ApiResponse> handleUsernameAlreadyExist(
--- src/main/java/com/bpi/training/module10/mapper/UserMapper.java
+++ src/main/java/com/bpi/training/module10/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.bpi.training.module10.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.bpi.training.module10.dto.UserDTO;
 import com.bpi.training.module10.entity.User;
@@ -8,8 +9,11 @@ import com.bpi.training.module10.entity.User;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+
+    @Mapping(target = "roles", ignore = true)
     User toEntity(UserDTO userDto);
 
+    @Mapping(target = "role", ignore = true)
     UserDTO toDto(User user);
     
 } 
